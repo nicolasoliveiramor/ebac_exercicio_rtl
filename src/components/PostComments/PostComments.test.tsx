a0ea18adedd103b710b39867b73d394e82584002
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2025-01-03 21:12:51 -0300

    Adicionada testes para comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,32 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('Deve adicionar dois comentarios', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('comment-area'), { 
+            target: { 
+                value: 'Comentario aleatorio' 
+            } 
+        });
+        
+        fireEvent.click(screen.getByTestId('confirma-comment'));
+
+
+        fireEvent.change(screen.getByTestId('comment-area'), { 
+            target: { 
+                value: 'Segundo Comentario aleatorio' 
+            } 
+        });        
+        
+        fireEvent.click(screen.getByTestId('confirma-comment'));
+
+        expect(screen.getAllByTestId('comments')).toHaveLength(2);
     });
 });
