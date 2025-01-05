13cb05fe2c1279968ece7587233ce353bdb67f89
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2025-01-04 22:04:33 -0300

    fix: pastas de testes

--- src/components/PostComments/__tests__/PostComments.test.tsx
+++ src/components/PostComments/__tests__/PostComments.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import PostComment from '.';
+import PostComment from '..';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
